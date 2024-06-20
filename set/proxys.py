5c2c43ab6074ea9b2778530e1c6208cf698f1daa
ERupBOU <140595493+ERupBOU@users.noreply.github.com>
2024-06-20 22:31:59 +0700

    Update proxys.py

--- set/proxys.py
+++ set/proxys.py
@@ -1,1 +1,9 @@
-.
+def getproxy():
+    proxy_host = input("Введите хост прокси-сервера: ")
+    proxy_port = input("Введите порт прокси-сервера: ")
+
+    with open("proxy.py", "w") as f:
+        f.write(f"PROXY_HOST = '{proxy_host}'\n")
+        f.write(f"PROXY_PORT = {proxy_port}\n")
+
+    print("Настройки прокси-сервера сохранены в файл proxy.py.")
